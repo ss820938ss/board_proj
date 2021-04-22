c32270b8d62902ece7e5cb5a5ab15facf2ea67db
parkyujin <ss820938ss@gmail.com>
2021-04-22 11:17:29 +0900

    end

--- src/main/java/board_proj/controller/BoardFrontController.java
+++ src/main/java/board_proj/controller/BoardFrontController.java
@@ -14,20 +14,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.ActionMap;
 
 import board_proj.action.Action;
-import board_proj.action.BoardDeleteFormAction;
-import board_proj.action.BoardDeleteProAction;
-import board_proj.action.BoardDetailAction;
-import board_proj.action.BoardFileDownAction;
-import board_proj.action.BoardListAction;
-import board_proj.action.BoardModifyFormAction;
-import board_proj.action.BoardModifyProAction;
-import board_proj.action.BoardReplyFormAction;
-import board_proj.action.BoardReplyProAction;
-import board_proj.action.BoardWriteFormAction;
-import board_proj.action.BoardWriteProAction;
+import board_proj.action.NullAction;
 import board_proj.dto.ActionForward;
 
 @WebServlet(urlPatterns = {"*.do"},
@@ -44,25 +33,25 @@ public class BoardFrontController extends HttpServlet {
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("init() - config " + config.getInitParameter("configFile"));
 		String configFile = config.getInitParameter("configFile");
 		try(InputStream is = config.getServletContext().getResourceAsStream(configFile)){
 			Properties props = new Properties();
 			props.load(is);
 			
-			System.out.println("props "  + props);
 			for(Entry<Object, Object> entry : props.entrySet()) {
-//				System.out.println(entry.getKey() + " : " + entry.getValue());
-				Class<?> cls = Class.forName((String)entry.getValue());
-				Action action = (Action) cls.newInstance();
+				Class<?> cls;
+				Action action = null;
+				try {
+					cls = Class.forName((String)entry.getValue());
+					action = (Action) cls.newInstance();
+				} catch (ClassNotFoundException e) {
+					action = new NullAction();
+					e.printStackTrace();
+				}
 				actionMap.put((String)entry.getKey(), action);
 			}
-			
-//			for(Entry<String, Action> entry : actionMap.entrySet()) {
-//				System.out.println(entry.getKey() + " : " + entry.getValue());
-//			}
-			
-		} catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+
+		} catch (IOException | InstantiationException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
 	}
@@ -78,54 +67,9 @@ public class BoardFrontController extends HttpServlet {
 	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String command = request.getServletPath();
 		
-		System.out.println(command);
-		
-		ActionForward forward=null;
-		Action action = null;
-		
-		action = actionMap.get(command);
-		forward = action.execute(request, response);
-		
-/*		try {
-			
-			if (command.equals("/boardWriteForm.do")) {
-				action = new BoardWriteFormAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardWritePro.do")) {
-				action = new BoardWriteProAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardList.do")) {
-				action = new BoardListAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardDetail.do")) {
-				action = new BoardDetailAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardDeleteForm.do")) {
-				action = new BoardDeleteFormAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardDeletePro.do")) {
-				action = new BoardDeleteProAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/file_down.do")) {
-				action = new BoardFileDownAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardModifyForm.do")) {
-				action = new BoardModifyFormAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardModifyPro.do")) {
-				action = new BoardModifyProAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardReplyForm.do")) {
-				action = new BoardReplyFormAction();
-				forward = action.execute(request, response);
-			}else if (command.equals("/boardReplyPro.do")) {
-				action = new BoardReplyProAction();
-				forward = action.execute(request, response);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-*/		
+		Action action = actionMap.get(command);
+		ActionForward forward = action.execute(request, response);
+
 		if (forward != null) {
 			if (forward.isRedirect()) {
 				response.sendRedirect(forward.getPath());
---
+++ src/main/webapp/board/nullAction.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Null Error</title>
+</head>
+<body>
+객체 미생성
+</body>
+</html>
